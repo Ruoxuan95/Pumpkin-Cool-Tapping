47a82c9ec80b4b3a6f5ea56a5a2d83fdb0c7397a
闫晓星 <yanxiaoxing@dhcp-rhodes-842.redrover.cornell.edu>
2017-11-06 18:27:54 -0500

    keyboard create

---
+++ keyboard.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+
+def key_initiate(all_pin)：
+    GPIO.setmode(GPIO.BCM)
+    [GPIO.setup(pin,GPIO.IN,pull_up_down=GPIO.PUD_UP) for pin in all_pin]
+
+       
+def key_status(all_pin):
+    return [ not GPIO.input(pin) for pin in all_pin]		
+			
+			
+def key_clean():
+    GPIO.cleanup()
+		
